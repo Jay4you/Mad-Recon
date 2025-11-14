998b2e31d9de7db2444d08addf8229e86ecc7351
Ugochukwu Eke <ejahswill1@gmail.com>
2025-11-14 22:50:00 +0100

    updated the setup

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y subfinder httpx nuclei naabu dnsx jq python3 python3-pip
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
